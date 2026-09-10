38c6a7e4dd9cb24491b3ca0eee14272c200b984d
liguobao <codelover@qq.com>
2026-09-10 16:20:13 +0800

    chore: release remote plugin 0.4.20

--- packages/plugin/dist/index.js
+++ packages/plugin/dist/index.js
@@ -17493,7 +17493,7 @@ function normalizeServerUrl(value) {
 }
 
 // src/version.ts
-var PLUGIN_VERSION = "0.4.19";
+var PLUGIN_VERSION = "0.4.20";
 
 // src/server-api.ts
 var TERMINAL_CONTROL_CHARACTERS = /[\u0000-\u001f\u007f-\u009f]/u;
